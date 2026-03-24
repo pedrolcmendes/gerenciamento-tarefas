69c1d1c274fd355c11fbc27e776dcac22d870e53
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 17:25:21 -0300

    add TarefaService com operações CRUD

---
+++ tarefas/src/main/java/com/exercicio/tarefas/service/TarefaService.java
@@ -0,0 +1,34 @@
+package com.exercicio.tarefas.service;
+
+import com.exercicio.tarefas.entity.Tarefa;
+import com.exercicio.tarefas.repository.TarefaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TarefaService {
+
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository) {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    public Tarefa salvar(Tarefa tarefa) {
+        return tarefaRepository.save(tarefa);
+    }
+
+    public List<Tarefa> listarTodos() {
+        return tarefaRepository.findAll();
+    }
+
+    public Optional<Tarefa> buscarPorId(Long id) {
+        return tarefaRepository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        tarefaRepository.deleteById(id);
+    }
+}
