a7b3e76e4b2050af5181e89085d88b87bdbebdf7
pedrolcmendes <pepelmendes05@gmail.com>
2026-03-24 17:27:37 -0300

    add controller REST com endpoints

---
+++ tarefas/src/main/java/com/exercicio/tarefas/controller/TarefaController.java
@@ -0,0 +1,43 @@
+package com.exercicio.tarefas.controller;
+
+import com.exercicio.tarefas.entity.Tarefa;
+import com.exercicio.tarefas.service.TarefaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tarefas")
+public class TarefaController {
+
+    private final TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService) {
+        this.tarefaService = tarefaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Tarefa> criar(@RequestBody Tarefa tarefa) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(tarefaService.salvar(tarefa));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Tarefa>> listarTodos() {
+        return ResponseEntity.ok(tarefaService.listarTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Tarefa> buscarPorId(@PathVariable Long id) {
+        return tarefaService.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        tarefaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+}
